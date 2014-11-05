56020110b1a51573355c4f894e26bacbaa8333b4
Marcel de Castilho <marcel.castilho@gmail.com>
2014-11-05 14:54:54 -0200

    by Adrisson, Bassani, Champs, Geciane, Heitor e Marcel

---
+++ Sodoku.Tests/SolverTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+namespace Sodoku.Tests {
+    public class SolverTests {
+        public const string Board = @"+-------+-------+-------+
+                                      | _ 6 _ | 1 _ 4 | _ 5 _ |
+                                      | _ _ 8 | 3 _ 5 | 6 _ _ |
+                                      | 2 _ _ | _ _ _ | _ _ 1 |
+                                      +-------+-------+-------+
+                                      | 8 _ _ | 4 _ 7 | _ _ 6 |
+                                      | _ _ 6 | _ _ _ | 3 _ _ |
+                                      | 7 _ _ | 9 _ 1 | _ _ 4 |
+                                      +-------+-------+-------+
+                                      | 5 _ _ | _ _ _ | _ _ 2 |
+                                      | _ _ 7 | 2 _ 6 | 9 _ _ |
+                                      | _ 4 _ | 5 _ 8 | _ 7 _ |
+                                      +-------+-------+-------+";
+
+        private Solver _solver;
+
+        [SetUp]
+        public void SetUp() {
+            _solver = new Solver(Board);
+        }
+
+        [Test]
+        public void Deve_inicializar_solver_9x9() {
+            Assert.AreEqual(9, _solver.Board.Length);
+            Assert.AreEqual(9, _solver.Board[0].Length);
+        }
+
+        [Test]
+        public void Deve_inicializar_preenchidos() {
+            Assert.AreEqual(6, _solver.Board[0][1]);
+            Assert.AreEqual(1, _solver.Board[5][5]);
+        }
+
+        [Test]
+        public void Deve_inicializar_nao_preenchidos_com_zero() {
+            Assert.AreEqual(0, _solver.Board[0][0]);
+            Assert.AreEqual(0, _solver.Board[8][8]);
+        }
+
+        [Test]
+        public void Deve_retornar_linha_0() {
+            CollectionAssert.AreEqual(new[] { 0, 6, 0, 1, 0, 4, 0, 5, 0 }, _solver.GetRow(0));
+        }
+
+        [Test]
+        public void Deve_retornar_coluna_0() {
+            CollectionAssert.AreEqual(new[] { 0, 0, 2, 8, 0, 7, 5, 0, 0 }, _solver.GetColumn(0));
+        }
+
+        [TestCase(0, new[] { 0, 6, 0, 0, 0, 8, 2, 0, 0 })]
+        [TestCase(1, new[] { 1, 0, 4, 3, 0, 5, 0, 0, 0 })]
+        [TestCase(4, new[] { 4, 0, 7, 0, 0, 0, 9, 0, 1 })]
+        [TestCase(8, new[] { 0, 0, 2, 9, 0, 0, 0, 7, 0 })]
+        public void Deve_retornar_caixa(int box, int[] esperado) {
+            CollectionAssert.AreEqual(esperado, _solver.GetBox(box));
+        }
+
+        [TestCase(0, 0, new[] { 3, 9 })]
+        [TestCase(0, 2, new[] { 3, 9 })]
+        public void Deve_encontrar_os_valores_validos_para_uma_posicao(int row, int column, int[] esperado) {
+            CollectionAssert.AreEqual(esperado, _solver.GetPossibleValues(row, column));
+        }
+
+        [Test]
+        public void Deve_resolver() {
+            Assert.IsTrue(_solver.Solve());
+        }
+    }
+}
---
+++ Sodoku/Solver.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Sodoku {
+    public class Solver {
+        public int[][] Board { get; set; }
+
+        public Solver(string board) {
+            Board = new int[9][];
+            Load(board);
+        }
+
+        private void Load(string board) {
+            var regex = new Regex(@"[^0-9_]", RegexOptions.None);
+            var clean = regex.Replace(board, "");
+            var i = 0;
+            while (i < 9) {
+                Board[i] = clean.Skip(i * 9).Take(9).Select(x => x != '_' ? Int32.Parse(x.ToString()) : 0).ToArray();
+                i++;
+            }
+        }
+
+        public int[] GetRow(int row) {
+            return Board[row];
+        }
+
+        public int[] GetColumn(int column) {
+            var result = new int[9];
+            for (var i = 0; i < 9; i++) {
+                result[i] = Board[i][column];
+            }
+            return result;
+        }
+
+        public bool Solve() {
+            PreencherPosicao(0, 0);
+            PrintBoard();
+            return false;
+        }
+
+        private void PreencherPosicao(int rowInicio, int colInicio) {
+            for (int row = rowInicio; row < 9; row++) {
+                for (int column = colInicio; column < 9; column++) {
+                    if (Board[row][column] == 0) {
+                        var valoresPossiveis = GetPossibleValues(row, column);
+                        for (int i = 0; i < valoresPossiveis.Count(); i++) {
+                            Board[row][column] = valoresPossiveis[i];
+                            PrintBoard();
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            PreencherPosicao(rowInicio, colInicio + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PrintBoard() {
+            Console.Write(String.Join(Environment.NewLine, Board.Select(row => String.Join(" ", row.Select(x => x > 0 ? x.ToString() : "_")))));
+        }
+
+        public int[] GetBox(int box) {
+            int row, column;
+
+            switch (box) {
+                case 0:
+                    row = 0;
+                    column = 0;
+                    break;
+                case 1:
+                    row = 0;
+                    column = 3;
+                    break;
+                case 2:
+                    row = 0;
+                    column = 6;
+                    break;
+                case 3:
+                    row = 3;
+                    column = 0;
+                    break;
+                case 4:
+                    row = 3;
+                    column = 3;
+                    break;
+                case 5:
+                    row = 3;
+                    column = 6;
+                    break;
+                case 6:
+                    row = 6;
+                    column = 0;
+                    break;
+                case 7:
+                    row = 6;
+                    column = 3;
+                    break;
+                case 8:
+                    row = 6;
+                    column = 6;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            return new[]{                
+                Board[row + 0][column + 0],
+                Board[row + 0][column + 1],
+                Board[row + 0][column + 2],
+                Board[row + 1][column + 0],
+                Board[row + 1][column + 1],
+                Board[row + 1][column + 2],
+                Board[row + 2][column + 0],
+                Board[row + 2][column + 1],
+                Board[row + 2][column + 2]
+            };
+        }
+
+        public int[] GetPossibleValues(int row, int column) {
+            var result = new List<int>();
+            for (var i = 1; i <= 9; i++) {
+                if (!GetRow(row).Contains(i)
+                && !GetColumn(column).Contains(i)
+                && !GetBox(GetBoxIndex(row, column)).Contains(i)) {
+                    result.Add(i);
+                }
+            }
+            return result.ToArray();
+        }
+
+        private int GetBoxIndex(int row, int column) {
+            return (row - (row % 3)) + (column / 3);
+        }
+    }
+}
